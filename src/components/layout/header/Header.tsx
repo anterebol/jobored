3ab1dbed3a4f813b3a843de73c0c7e5849ea11a5
unknown <anterebol@gmail.com>
2023-05-05 14:19:57 +0300

    feat: update header component

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,12 +1,25 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { FC } from 'react';
+import styles from './header.module.css';
+import logo from '@/assets/jobored_logo.svg';
 
 export const Header: FC = () => {
   return (
-    <header className="header">
-      <div className="header-logo"></div>
-      <Link href={'/vacancies'}>Поиск Вакансий</Link>
-      <Link href={'/favorits'}>Избранное</Link>
+    <header className={styles.header}>
+      <div className={styles['header-container']}>
+        {/* <div className={styles['header-logo']}></div> */}
+        <Image src={logo} width={141} height={36} alt="logo" />
+        {/* <Image src={styles['.header-logo']} src='../../../assets/jobored_logo.svg' ></Image> */}
+        <div className={styles['header-links']}>
+          <Link href={'/vacancies'} className={styles['header-links__item']}>
+            Поиск Вакансий
+          </Link>
+          <Link href={'/favorits'} className={(styles['header-links__item'], styles.left)}>
+            Избранное
+          </Link>
+        </div>
+      </div>
     </header>
   );
 };
