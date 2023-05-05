21724c09b46c0c970592dc86cfeba1519d0e71cb
unknown <anterebol@gmail.com>
2023-05-05 21:20:05 +0300

    feat: add server fetch for catalogues

--- src/pages/vacancies/index.tsx
+++ src/pages/vacancies/index.tsx
@@ -1,12 +1,18 @@
 import { getToken, getVacancies } from '@/api/api';
-import { Layout } from '@/components/layout/Layout';
-import { FilterVacanciesType, VacancyType } from '@/types/types';
+import { CatalougeType, FilterVacanciesType, VacancyType } from '@/types/types';
 import { NextPage } from 'next';
+import { FormVacancy } from '@/components/vacancies/form/Form';
 import { useEffect, useState } from 'react';
+import styles from '../styles/Vacancies.module.css';
+import { Vacancy } from '@/components/vacancies/Vacancy';
+import { cataloguesPath } from '@/constants/path';
+import { createPath } from '@/utils/createPath';
+import { headers } from '@/constants/headers';
+import { url } from '@/constants/url';
 
-const Vacancies: NextPage = () => {
+const Vacancies = ({ catalogues }: { catalogues: Array<CatalougeType> }) => {
+  console.log(catalogues);
   // const [catalogues, setCatalogues] = useState([]);
-
   const [vacancies, setVacancies] = useState([] as Array<VacancyType>);
   useEffect(() => {
     const getData = async () => {
@@ -26,13 +32,38 @@ const Vacancies: NextPage = () => {
     getData();
   }, []);
   return (
-    <>
-      <h1>Vacancies</h1>
-      {vacancies.map((vacancy: VacancyType) => {
-        return <h1 key={vacancy.id}>{vacancy.id}</h1>;
-      })}
-    </>
+    <div className={styles['vacancies-page']}>
+      <div className={styles['left-bar']}>
+        <FormVacancy catalogues={catalogues} />
+      </div>
+      <div className={styles['right-bar']}></div>
+
+      <ul>
+        {vacancies.map((vacancy: VacancyType) => {
+          return (
+            <li key={vacancy.id}>
+              <Vacancy vacancy={vacancy} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
+
+export async function getServerSideProps() {
+  const path = createPath(url, cataloguesPath, undefined);
+  const response = await fetch(path, {
+    method: 'GET',
+    headers: headers,
+    redirect: 'follow',
+  });
+  const data = await response.json();
+  return {
+    props: {
+      catalogues: data,
+    },
+  };
+}
 
 export default Vacancies;
