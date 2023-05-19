427cdff0f4f82d0c2fbbda33d5cb928f98d5ef68
unknown <anterebol@gmail.com>
2023-05-20 00:08:19 +0300

    feat: add adaptive to header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import styles from './header.module.css';
 import logo from '@/assets/jobored_logo.svg';
+import nav from '@/assets/nav.svg';
 import { useAppSelector } from '@/hooks/hooks';
 import { useRouter } from 'next/router';
 import { isActiveLink } from '@/utils/isActiveLink';
@@ -11,11 +12,21 @@ import { FAVORITES_PATH, VACANCIES_PATH } from '@/constants/query/path';
 export const Header: FC = () => {
   const { currentPage } = useAppSelector((state) => state);
   const path = useRouter().pathname;
+
+  const [mobileOpened, setMobileOpened] = useState('');
+
+  const openMobileNav = () => {
+    if (!mobileOpened) {
+      setMobileOpened('opened');
+    } else {
+      setMobileOpened('');
+    }
+  };
   return (
     <header className={styles.header}>
       <div className={styles['header-container']}>
         <Image src={logo} width={141} height={36} alt="logo" />
-        <div className={styles['header-links']}>
+        <nav className={styles['header-links']}>
           <Link
             href={`/${VACANCIES_PATH}/page=${currentPage.vacancies}`}
             className={[
@@ -35,6 +46,33 @@ export const Header: FC = () => {
           >
             Избранное
           </Link>
+        </nav>
+        <div className={[styles['mobile-navigation'], styles[mobileOpened]].join(' ')}>
+          <button className={styles['nav-btn']} onClick={openMobileNav}>
+            <Image src={nav} alt={'nav-icon'} width={30} height={30} />
+          </button>
+          <nav className={styles['mobile-links']}>
+            <Link
+              onClick={openMobileNav}
+              href={`/${VACANCIES_PATH}/page=${currentPage.vacancies}`}
+              className={[
+                styles['mobile-links__item'],
+                styles[isActiveLink([path, VACANCIES_PATH])],
+              ].join(' ')}
+            >
+              Поиск Вакансий
+            </Link>
+            <Link
+              onClick={openMobileNav}
+              href={`/${FAVORITES_PATH}/page=${currentPage.favorites}`}
+              className={[
+                styles['mobile-links__item'],
+                styles[isActiveLink([path, FAVORITES_PATH])],
+              ].join(' ')}
+            >
+              Избранное
+            </Link>
+          </nav>
         </div>
       </div>
     </header>
